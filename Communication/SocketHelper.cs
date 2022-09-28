dfb49312534d5bd8345cc28fde123b7cb276ce23
Mathias Castiñeiras <mathias.castineiras@innovant.us>
2022-09-28 16:27:03 -0300

    Add Communication project to handle Files

---
+++ Communication/SocketHelper.cs
@@ -0,0 +1,56 @@
+﻿using System;
+using System.Net.Sockets;
+
+namespace Communication
+{
+    public class SocketHelper
+    {
+        private readonly Socket _socket;
+
+        public SocketHelper(Socket socket)
+        {
+            this._socket = socket;
+        }
+
+        public void Send(byte[] data)
+        {
+            int offset = 0;
+            while (offset < data.Length)
+            {
+                var sent = this._socket.Send(
+                    data,
+                    offset,
+                    data.Length - offset,
+                    SocketFlags.None);
+                if (sent == 0)
+                {
+                    throw new Exception("Connection lost");
+                }
+
+                offset += sent;
+            }
+        }
+
+        public byte[] Receive(int length)
+        {
+            int offset = 0;
+            var data = new byte[length];
+            while (offset < length)
+            {
+                var received = this._socket.Receive(
+                    data,
+                    offset,
+                    length - offset,
+                    SocketFlags.None);
+                if (received == 0)
+                {
+                    throw new Exception("Connection lost");
+                }
+
+                offset += received;
+            }
+
+            return data;
+        }
+    }
+}
