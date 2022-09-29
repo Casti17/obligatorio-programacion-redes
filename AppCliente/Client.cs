010e66d7350abe23c1815084aa55e747e2b3671d
Nicolás Varela <nicovar@adinet.com.uy>
2022-09-29 01:16:05 -0300

    Cambio dependencia

--- AppCliente/Client.cs
+++ AppCliente/Client.cs
@@ -1,17 +1,19 @@
-﻿using Protocolo;
+﻿using Communication;
 using System;
 using System.Net;
 using System.Net.Sockets;
-using System.Text;
+using System.Text.RegularExpressions;
 
 namespace AppCliente
 {
     public class Client
     {
         private static readonly SettingsManager settingsMngr = new SettingsManager();
+        public static ClientHandler clientHandler = new ClientHandler();
 
         private static void Main(string[] args)
         {
+            bool connected = true;
             Console.WriteLine("Iniciando Aplicacion Cliente....!!!");
 
             var socketCliente = new Socket(
@@ -28,95 +30,57 @@ namespace AppCliente
             socketCliente.Connect(serverEndpoint);
             Console.WriteLine("Cliente Conectado al Servidor...!!!");
 
-            Console.WriteLine("\nBienvenido al Sistema Client");
-            Console.WriteLine("Opciones validas: ");
-            Console.WriteLine("Alta de usuario");
-            Console.WriteLine("Alta de perfil de trabajo ");
-            Console.WriteLine("Asociar foto a perfil de trabajo");
-            Console.WriteLine("Consultar perfiles existentes");
-            Console.WriteLine("Consultar perfil especifico");
-            Console.WriteLine("Mensajes");
-            Console.WriteLine("Configuracion");
-            Console.WriteLine("Exit");
+            Console.WriteLine("\nWelcome to the LkDin");
+            Console.WriteLine("Please, select one of the valid options: ");
+            Console.WriteLine("1- Create user");
+            Console.WriteLine("2- Create work profile");
+            Console.WriteLine("3- Update picture");
+            Console.WriteLine("4- Consultar perfiles existentes");
+            Console.WriteLine("5- Consultar perfil especifico");
+            Console.WriteLine("6- Enviar mensaje");
+            Console.WriteLine("7- Check inbox");
+            Console.WriteLine("8- Disconnect");
             Console.WriteLine("Ingrese su opcion: ");
-            bool parar = false;
-            while (!parar)
+            while (connected)
             {
                 String mensaje = Console.ReadLine();
                 switch (mensaje)
                 {
-                    case "alta usuario":
-
+                    case "1":
+                        CaseCreateUser(socketCliente);
                         break;
 
-                    case "Alta de perfil de trabajo":
-                        //CaseUpProfile();
-
+                    case "2":
+                        CaseCreateWorkProfile();
                         break;
 
-                    case "Asociar foto a perfil de trabajo":
+                    case "3":
+                        CaseUpdateImageWorkProfile();
+                        break;
+
+                    case "4":
+                        CaseSearchProfileWithFilter();
+                        break;
+
+                    case "5":
+                        CaseSearchProfile();
+                        break;
+
+                    case "6":
+                        CaseSendMessage();
+                        break;
+
+                    case "7":
+                        CaseCheckInbox();
+                        break;
+
+                    default:
+                        Console.WriteLine("Opcion Invalida");
                         break;
                 }
                 if (mensaje.Equals("Exit", StringComparison.InvariantCultureIgnoreCase))
                 {
-                    parar = true;
-                }
-                else
-                {
-                    try
-                    {
-                        byte[] data = Encoding.UTF8.GetBytes(mensaje);
-                        byte[] dataLength = BitConverter.GetBytes(data.Length);
-
-                        // Mando primero el tamaño
-                        int offset = 0;
-                        int size = Constantes.LargoFijo;
-                        while (offset < size)
-                        {
-                            int enviados = socketCliente.Send(dataLength, offset, size - offset, SocketFlags.None);
-                            if (enviados == 0)
-                            {
-                                throw new SocketException();
-                            }
-                            offset += enviados;
-                        }
-
-                        // Mando el mensaje
-                        offset = 0;
-                        size = data.Length;
-                        while (offset < size)
-                        {
-                            int enviados = socketCliente.Send(data, offset, size - offset, SocketFlags.None);
-                            if (enviados == 0)
-                            {
-                                throw new SocketException();
-                            }
-                            offset += enviados;
-                        }
-
-                        // El cliente recibe la respuesta
-                        byte[] datarespuestaLength = new byte[Constantes.LargoFijo];
-                        int recibido = socketCliente.Receive(datarespuestaLength);
-                        if (recibido == 0)
-                        {
-                            throw new SocketException();
-                        }
-
-                        byte[] datarespuesta = new byte[BitConverter.ToInt32(dataLength, 0)];
-                        // en Visual Studio no es necesario el parametro 0, solo con el buffer es suficiente
-                        recibido = socketCliente.Receive(datarespuesta);
-                        if (recibido == 0)
-                        {
-                            throw new SocketException();
-                        }
-                        string respuesta = Encoding.UTF8.GetString(datarespuesta);
-                        Console.WriteLine("El servidor respondio: {0}", respuesta);
-                    }
-                    catch (SocketException)
-                    {
-                        Console.WriteLine("Se perdio la conexion con el Servidor");
-                        parar = true;
-                    }
+                    //parar = true;
                 }
             }
 
@@ -126,8 +90,96 @@ namespace AppCliente
             socketCliente.Close();
         }
 
-        private static void CaseUpUser()
+        public static void CaseCreateUser(Socket socketCliente)
         {
+            Console.WriteLine("Please input the following information. NO SPACES ALLOWED");
+            Console.WriteLine("Username?");
+            var userName = Console.ReadLine();
+            bool correctFormat = Regex.IsMatch(userName, @"^[a-zA-Z]+$");
+            while (!correctFormat)
+            {
+                Console.WriteLine("Incorrect format, please try again.");
+                Console.WriteLine("Name?");
+                userName = Console.ReadLine();
+                correctFormat = Regex.IsMatch(userName, @"^[a-zA-Z]+$");
+            }
+            Console.WriteLine("Name?");
+            var name = Console.ReadLine();
+            correctFormat = Regex.IsMatch(name, @"^[a-zA-Z]+$");
+            while (!correctFormat)
+            {
+                Console.WriteLine("Incorrect format, please try again");
+                Console.WriteLine("Name?");
+                name = Console.ReadLine();
+                correctFormat = Regex.IsMatch(name, @"^[a-zA-Z]+$");
+            }
+
+            Console.WriteLine("Last name?");
+            var lastName = Console.ReadLine();
+            correctFormat = Regex.IsMatch(lastName, @"^[a-zA-Z]+$");
+            while (!correctFormat)
+            {
+                Console.WriteLine("Incorrect format, please try again");
+                Console.WriteLine("Last name?");
+                lastName = Console.ReadLine();
+                correctFormat = Regex.IsMatch(name, @"^[a-zA-Z]+$");
+            }
+            string user = $"{name}#{lastName}#{userName}";
+            //clientHandler.CreateUser(user);
+        }
+
+        public static void CaseCreateWorkProfile()
+        {
+            Console.WriteLine("Please provide us with a description");
+            var description = Console.ReadLine();
+            Console.WriteLine("Please list your skills/abilities:");
+            var skill = Console.ReadLine();
+            while (!skill.Equals("No"))
+            {
+                skill += skill;
+                Console.WriteLine("If you do not have more abilities, type No");
+            }
+        }
+
+        private static void CaseUpdateImageWorkProfile()
+        {
+            throw new NotImplementedException();
+        }
+
+        private static void CaseSearchProfile()
+        {
+            Console.WriteLine("Please insert the username you want to search");
+            var filter = Console.ReadLine();
+        }
+
+        private static void CaseSearchProfileWithFilter()
+        {
+            Console.WriteLine("Which parameter would you like to search by?");
+            Console.WriteLine("Abilities - Word");
+            var filter = Console.ReadLine();
+            switch (filter)
+            {
+                case "Abilities":
+                    Console.WriteLine("Please insert the ability you would like to search");
+                    var ability = Console.ReadLine();
+                    //buscar por ability.
+                    break;
+
+                case "Word":
+                    Console.WriteLine("Please insert the word you would like to search");
+                    var word = Console.ReadLine();
+                    break;
+            }
+        }
+
+        private static void CaseSendMessage()
+        {
+            Console.WriteLine("");
+        }
+
+        private static void CaseCheckInbox()
+        {
+            throw new NotImplementedException();
         }
     }
 }
