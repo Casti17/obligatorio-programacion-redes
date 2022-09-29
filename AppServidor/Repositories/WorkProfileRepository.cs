398e8a304d5e64e1b4ee72e0f86cee8899d1f2d5
Nicolás Varela <nicovar@adinet.com.uy>
2022-09-29 00:28:22 -0300

    Read

--- AppServidor/Repositories/WorkProfileRepository.cs
+++ AppServidor/Repositories/WorkProfileRepository.cs
@@ -26,5 +26,31 @@ namespace Repositories
             }
             return profile;
         }
+
+        public static IList<WorkProfile> GetProfilesBySkills(string skill)
+        {
+            IList<WorkProfile> workProfiles = new List<WorkProfile>();
+            foreach (var profile in _workProfiles)
+            {
+                if (profile.Skills.Contains(skill))
+                {
+                    workProfiles.Add(profile);
+                }
+            }
+
+            return workProfiles;
+        }
+        public static IList<WorkProfile> GetProfilesByKeyWord(string keyWord)
+        {
+            IList<WorkProfile> workProfiles = new List<WorkProfile>();
+            foreach (var profile in _workProfiles)
+            {
+                if (profile.Description.Contains(keyWord))
+                {
+                    workProfiles.Add(profile);
+                }
+            }
+            return workProfiles;
+        }
     }
 }
